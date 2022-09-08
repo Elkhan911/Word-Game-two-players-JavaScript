d2ffb5eaecbf1cf5430dfc2f2bb8e85bfff204ad
Elkhan911 <955pro@mail.ru>
2022-09-08 20:36:48 +0500

    rewrited html, css and js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const inputForLetter = document.querySelector("#_inputForLetter");
+
 const input = document.querySelector("#_input");
 const user1Answer = document.querySelector("#_user1Answer");
 const user2Answer = document.querySelector("#_user2Answer");
@@ -5,12 +7,13 @@ const gameHint = document.querySelector("#_gameHint");
 const timer = document.querySelector("#_timer");
 const resetBtn = document.querySelector("#_resetBtn");
 const hideBtn = document.querySelector("#_hideBtn");
-const answersZone = document.querySelector("#_answersZone");
 const showConditionsBtn = document.querySelector("#_showConditionsBtn");
 const gameRules = document.querySelector("#_gameRules");
-const inputForletter = document.querySelector("#_inputForletter");
 const choosenLetter = document.querySelector("#_choosenLetter");
-const letterChooseZone = document.querySelector("#_letterChooseZone");
+const gameContainer1 = document.querySelector("#_gameContainer1");
+const gameContainer2 = document.querySelector("#_gameContainer2");
+const gameContainer3 = document.querySelector("#_gameContainer3");
+const gameContainerAnswers = document.querySelector("#_gameContainerAnswers");
 const chooseUser = document.querySelector("#_chooseUser");
 
 // массив для ответов юзера 1
@@ -20,31 +23,24 @@ let arrAnswers1P = [];
 let arrAnswers2P = [];
 
 // переменная для определения списка ответов юзеров
-let firstPlayer = true;
+let firstPlayerTurn = true;
 
 // переменная айди таймера для аннулирования таймера
 let timerId;
 
 // счетчик для таймера
-let timerCounter = 121;
+let timerCounter = 120;
 
 // переменнная для кнопки скрыть ответы
 let showAnswers = true;
 
+// переменная для скрытий условий игры по умолчанию
 let showConditions = false;
 // functions
 
-// функция для проверки повторного использования слова
-function checkRepeat(word) {
-  if (arrAnswers1P.includes(word) || arrAnswers2P.includes(word)) {
-    alert("Это слово уже было");
-    return true;
-  }
-}
-
 // функция для проверки длины ввода
 function checkLength(inputValue) {
-  inputValueSplit = inputValue.split("");
+  let inputValueSplit = inputValue.split("");
   if (inputValueSplit.length > 1) {
     inputValue.value = "";
     alert("Нужно ввести одну букву для игры");
@@ -53,48 +49,84 @@ function checkLength(inputValue) {
   } else return true;
 }
 
-// слушатель для ввода буквы
-inputForletter.addEventListener("keydown", function (event) {
+function checkFirstLetter(str, firstLetter) {
+  if (str[0].toLowerCase() == firstLetter.toLowerCase()) {
+    return true;
+  } else {
+    alert("Вы вводите слово не на ту букву");
+  }
+}
+
+// функция для проверки повторного использования слова
+function checkRepeat(word) {
+  if (arrAnswers1P.includes(word) || arrAnswers2P.includes(word)) {
+    alert("Это слово уже было");
+    return false;
+  } else return true;
+}
+
+showConditionsBtn.addEventListener("click", function () {
+  if (!showConditions) {
+    gameRules.classList.remove("rules__texts_inactive");
+    this.textContent = "Скрыть условия игры";
+    showConditions = true;
+  } else {
+    gameRules.classList.add("rules__texts_inactive");
+    this.textContent = "Показать условия игры";
+    showConditions = false;
+  }
+});
+
+// слушатель для инпута с вводом буквы
+inputForLetter.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    if (checkLength(inputForletter.value)) {
-      choosenLetter.textContent = inputForletter.value.toUpperCase();
-      inputForletter.value = "";
-      letterChooseZone.classList.add("game__letter-choose_off");
-      chooseUser.classList.remove("game__hints_off");
+    if (checkLength(this.value)) {
+      choosenLetter.textContent = this.value.toUpperCase();
+      this.value = "";
+      gameContainer1.classList.add("game__container_inactive");
+      chooseUser.classList.remove("game__hint_inactive");
+
+      gameContainer2.classList.remove("game__container_inactive");
+      gameContainer3.classList.remove("game__container_inactive");
+      gameContainerAnswers.classList.remove("answers_inactive");
+
+      startTimer();
     }
   }
 });
 
+// слушатель для главного инпута игры
 input.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
     // проверяем не было ли уже использовано слово
-    if (checkFirstLetter(input.value, choosenLetter.textContent)) {
-      if (!checkRepeat(input.value)) {
-        if (firstPlayer) {
+    if (checkFirstLetter(this.value, choosenLetter.textContent)) {
+      if (checkRepeat(this.value)) {
+        if (firstPlayerTurn) {
           let newAnswer = document.createElement("p");
-          newAnswer.textContent = input.value;
-          newAnswer.classList.add("game__answerFirst");
+          newAnswer.textContent = this.value.toLowerCase();
+          newAnswer.classList.add("answers__text");
           user1Answer.append(newAnswer);
 
-          input.value = "";
+          this.value = "";
           arrAnswers1P.push(newAnswer.textContent);
-          console.log(arrAnswers1P);
+          // console.log(arrAnswers1P);
 
-          firstPlayer = false;
+          firstPlayerTurn = false;
           gameHint.textContent = 2;
         } else {
           let newAnswer = document.createElement("p");
-          newAnswer.textContent = input.value;
-          newAnswer.classList.add("game__answerSecond");
+          newAnswer.textContent = this.value.toLowerCase();
+          newAnswer.classList.add("answers__text");
           user2Answer.append(newAnswer);
 
-          input.value = "";
+          this.value = "";
           arrAnswers2P.push(newAnswer.textContent);
-          console.log(arrAnswers2P);
+          // console.log(arrAnswers2P);
 
-          firstPlayer = true;
+          firstPlayerTurn = true;
           gameHint.textContent = 1;
         }
+        timerCounter = 121;
       }
     }
   }
@@ -122,23 +154,21 @@ resetBtn.addEventListener("click", function () {
   arrAnswers1P = [];
   arrAnswers2P = [];
   clearInterval(timerId);
-  timer.innerHTML = " <u>кликните</u > для запуска таймера";
+  timer.innerHTML = 120;
   timer.addEventListener("click", startTimer);
 
-  letterChooseZone.classList.remove("game__letter-choose_off");
+  gameContainer1.classList.remove("game__container_inactive");
   chooseUser.classList.add("game__hints_off");
   choosenLetter.textContent = "";
 
-  // удаляем каждый созданный список с ответом юзера 1
-  let gameAnswerAll1 = document.querySelectorAll(".game__answerFirst");
-  for (let gameAnswer1 of gameAnswerAll1) {
-    user1Answer.removeChild(gameAnswer1);
-  }
-
-  // удаляем каждый созданный список с ответом юзера 2
-  let gameAnswerAll2 = document.querySelectorAll(".game__answerSecond");
-  for (let gameAnswer2 of gameAnswerAll2) {
-    user2Answer.removeChild(gameAnswer2);
+  // удаляем каждый созданный список с ответами
+  let gameAnswerAll = document.querySelectorAll(".answers__text");
+  for (let gameAnswer of gameAnswerAll) {
+    if (user1Answer.contains(gameAnswer)) {
+      user1Answer.removeChild(gameAnswer);
+    } else {
+      user2Answer.removeChild(gameAnswer);
+    }
   }
 });
 
@@ -156,24 +186,4 @@ hideBtn.addEventListener("click", function () {
     hideBtn.textContent = "скрыть ответы";
   }
   console.log(showAnswers);
-});
-
-function checkFirstLetter(str, firstLetter) {
-  if (str[0].toLowerCase() == firstLetter.toLowerCase()) {
-    return true;
-  } else {
-    alert("Вы вводите слово не на ту букву");
-  }
-}
-
-showConditionsBtn.addEventListener("click", function () {
-  if (!showConditions) {
-    gameRules.classList.remove("game__rules-zone_off");
-    this.textContent = "Скрыть условия игры";
-    showConditions = true;
-  } else {
-    gameRules.classList.add("game__rules-zone_off");
-    this.textContent = "Показать условия игры";
-    showConditions = false;
-  }
 });
